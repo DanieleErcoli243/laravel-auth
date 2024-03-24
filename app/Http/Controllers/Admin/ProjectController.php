87ae15865aa1af89d72d4ae942993d4398bcff9f
Polvere243 <danielercoli@gmail.com>
2024-03-24 13:53:55 +0100

    added a title via yield

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -13,7 +13,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.index');
+        $projects = Project::all();
+        return view('admin.index', compact('projects'));
     }
 
     /**
